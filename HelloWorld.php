38f675a17e8b8d5d2ac4b4980b6b3ebb72fa4bf1
Girmachew Gulint Tilahun <58398921+girmachew82@users.noreply.github.com>
2024-08-02 05:59:11 -0700

    Update HelloWorld.php

--- HelloWorld.php
+++ HelloWorld.php
@@ -3,8 +3,22 @@
 <body>
 
 <?php
-	
- 
- ?>
- </body>
+
+	echo "<h1 style='color:red'>Hello World </h1>";
+    $helloW = "Hello World";
+    echo "<br>";
+    echo "<span style = 'color:green'>$helloW</span>";
+    echo "<br>";
+    print($helloW);
+    echo "<br>";
+    print("Hello World");
+    echo "<hr>";
+    echo "<p> $helloW</p>";
+    echo "<p> $helloW</p>";
+    echo "<p> $helloW</p>";
+    echo "<p> $helloW</p>";
+?>
+
+
+</body>
 </html>
