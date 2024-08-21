02e2758ac09b689a4d0c9c6d2a824ce503ad7dac
Girmachew Gulint Tilahun <58398921+girmachew82@users.noreply.github.com>
2024-08-21 12:22:09 +0300

    Update orders.php

--- orders.php
+++ orders.php
@@ -6,9 +6,10 @@
     <title>Orders</title>
 </head>
 <body>
-    <h1>Customers Orders</h1>
+    <h1>Customers Orders </h1>
+    <p><a href="form.php">Add</a></p>
     <?php
-    $fp = fopen('orders.txt','rb');
+    $fp = fopen('orders.txt','a+');
     if(!$fp)
         echo "No file please try again later";
     while(!feof($fp)){
